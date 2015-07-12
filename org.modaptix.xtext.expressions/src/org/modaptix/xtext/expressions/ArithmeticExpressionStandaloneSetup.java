1457126c2dc68c3c066c93fa13140f5f3148277a
Max Hacking <max@hacking.co.uk>
2015-07-12 16:24:26 +0100

    chg: dev: Switched to declared ECore model.

--- org.modaptix.xtext.expressions/src/org/modaptix/xtext/expressions/ArithmeticExpressionStandaloneSetup.java
+++ org.modaptix.xtext.expressions/src/org/modaptix/xtext/expressions/ArithmeticExpressionStandaloneSetup.java
@@ -3,14 +3,29 @@
 */
 package org.modaptix.xtext.expressions;
 
-/**
- * Initialization support for running Xtext languages 
- * without equinox extension registry
- */
-public class ArithmeticExpressionStandaloneSetup extends ArithmeticExpressionStandaloneSetupGenerated{
+import org.eclipse.emf.ecore.EPackage;
+import com.google.inject.Injector;
 
-	public static void doSetup() {
+/**
+ * Initialization support for running Xtext languages without equinox extension
+ * registry
+ */
+public class ArithmeticExpressionStandaloneSetup extends ArithmeticExpressionStandaloneSetupGenerated
+{
+	public static void doSetup()
+	{
 		new ArithmeticExpressionStandaloneSetup().createInjectorAndDoEMFRegistration();
 	}
-}
 
+	@Override
+	public void register(Injector injector)
+	{
+		if (!EPackage.Registry.INSTANCE.containsKey("http://www.modaptix.org/xtext/expressions/ArithmeticExpression"))
+		{
+			EPackage.Registry.INSTANCE.put(
+					"http://www.modaptix.org/xtext/expressions/ArithmeticExpression",
+					org.modaptix.xtext.expressions.arithmeticExpression.ArithmeticExpressionPackage.eINSTANCE);
+		}
+		super.register(injector);
+	}
+}
